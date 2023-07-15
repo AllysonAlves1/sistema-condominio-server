a31ac605ea62e64d557b199f59e0bd46d5d46df6
Allyson Alves <allysonalves.01@gmail.com>
2023-07-15 13:03:20 -0300

    Configurando a variavel de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { PessoaModule } from './entidadesdentrodoap/pessoas/pessoa.module';
-import { Pessoa } from './entidadesdentrodoap/pessoas/pessoa.entity';
-import { Apartamento } from './apartamento/apartamento.entity';
-import { Usuario } from './usuarios/usuario.entity';
 import { ApartamentoModule } from './apartamento/apartamento.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './usuarios/auth/jwt.strategy';
@@ -15,26 +11,19 @@ import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './usuarios/auth/constants';
 import { UsuarioModule } from './usuarios/usuario.module';
 import { UsuarioService } from './usuarios/usuario.service';
-import { Veiculo } from './entidadesdentrodoap/veiculos/veiculo.entity';
-import { AcessoPessoa } from './acessos/acesso_pessoa/acessopessoa.entity';
-import { AcessoVeiculo } from './acessos/acesso_veiculo/acessoveiculo.entity';
 import { AcessoPessoaModule } from './acessos/acesso_pessoa/acessopessoa.module';
 import { AcessoVeiculoModule } from './acessos/acesso_veiculo/acessoveiculo.module';
 import { VeiculoModule } from './entidadesdentrodoap/veiculos/veiculo.module';
+import { DatabaseConfigService } from './config/database.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'admin',
-      password: 'admin',
-      database: 'nest',
-      entities: [Pessoa, Usuario, Apartamento, Veiculo, AcessoPessoa, AcessoVeiculo],
-      synchronize: true,
-      logging: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigService,
+      inject: [DatabaseConfigService],
     }),
     PessoaModule,
     ApartamentoModule,
@@ -46,7 +35,7 @@ import { VeiculoModule } from './entidadesdentrodoap/veiculos/veiculo.module';
     JwtModule.register({
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '60s' },
-    })
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, JwtStrategy, UsuarioService],
---
+++ src/config/database.config.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { AcessoPessoa } from 'src/acessos/acesso_pessoa/acessopessoa.entity';
+import { AcessoVeiculo } from 'src/acessos/acesso_veiculo/acessoveiculo.entity';
+import { Apartamento } from 'src/apartamento/apartamento.entity';
+import { Pessoa } from 'src/entidadesdentrodoap/pessoas/pessoa.entity';
+import { Veiculo } from 'src/entidadesdentrodoap/veiculos/veiculo.entity';
+import { Usuario } from 'src/usuarios/usuario.entity';
+
+@Injectable()
+export class DatabaseConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_USERNAME'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_NAME'),
+      entities: [
+        Pessoa,
+        Usuario,
+        Apartamento,
+        Veiculo,
+        AcessoPessoa,
+        AcessoVeiculo,
+      ],
+      synchronize: true,
+      logging: true,
+    };
+  }
+}
