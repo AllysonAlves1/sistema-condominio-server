29a35337c6e22eef00e1b417ade35fc9b41b6ba3
Allyson Alves <allysonalves.01@gmail.com>
2023-07-19 02:53:36 -0300

    Resolvendo o cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:3001', // Replace with the actual origin of your frontend application
+    origin: 'https://sistema-condominio.vercel.app/', // Replace with the actual origin of your frontend application
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
     methods: 'GET, POST, PUT, DELETE, OPTIONS',
   });
